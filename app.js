3c5dfa32cabfa38445e97be561fe92448df97d97
Nasser EDOU <edoubiyoonasser@gmail.com>
2024-06-03 17:02:10 +0100

    only send 5 poll quiz in a channel every 12 or 00

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const { getQuestion } = require("./questionsDB");
-const { sendMessage, sendPoll } = require("./axios");
+// const { getQuestion } = require("./questionsDB");
+const { /*sendMessage, */ sendPoll } = require("./axios");
 const fs = require("fs");
 const shuffle = require("shuffle-array");
 const app = express();
@@ -10,66 +10,88 @@ const port = process.env.NODE_PORT;
 app.use(express.json());
 
 app.post("/", (req, res) => {
-//   console.log(req.body);
-
-  var chatId = 0;
-  var messageText = "";
-
-  try {
-    chatId = req.body.message.chat.id;
-    messageText = req.body.message.text;
-  } catch (err) {
-    // console.log(err);
-  }
-
-  try {
-    chatId = req.body.poll_answer.user.id;
-  } catch (err) {
-    // console.log(err);
-  }
-
-  if (chatId === 0) return res.send("Mbolani!!!");
-
-  var axiosData = {};
-  axiosData.chat_id = chatId;
-
-  if (
-    messageText !== "/new_question" &&
-    messageText !== "new_question" &&
-    messageText !== "new"
-  ) {
-    axiosData.message =
-      'send or enter the command "/new_question" without the qotes to get a new question';
-    sendMessage(axiosData);
-  } else {
-    // const questionInf = getQuestion();
-    let content = JSON.parse(fs.readFileSync("questionsDB.json", "utf8"));
-
-    if (content.unusedQuestionsIndex.length === 0) {
-      for (var i = 0; i < content.questions.length; i++) {
-        content.unusedQuestionsIndex.push(i);
-      }
-    }
-
-    shuffle(content.unusedQuestionsIndex);
-    const questionIndex = content.unusedQuestionsIndex.pop();
-    axiosData.questionInf = content.questions[questionIndex];
-    console.log(`Number of question left: ${content.unusedQuestionsIndex.length}`)
-    sendPoll(axiosData);
-
-    fs.writeFileSync("questionsDB.json", JSON.stringify(content));
-  }
-
+  // console.log(req.body);
   res.send("Mbolani!!!");
 });
 
 app.get("/", (req, res) => {
-  console.log(req.body);
-
+  // console.log(req.body);
   res.send("this is a get");
 });
 
 app.listen(port, (err) => {
   if (err) console.log(err);
   console.log(`server app is listening on port ${port}`);
+
+  var dd = new Date();
+  // console.log(dd.getMinutes());
+  dd.getHours() === 12 || dd.getHours() === 0 ? -1 : sendQuestion();
+
+  setInterval(() => {
+    // sendQuestion();
+    var d = new Date();
+    // console.log(d.getHours() + "     " + (d.getHours() % 12));
+    dd.getHours() === 12 || dd.getHours() === 0 ? sendQuestion() : -1;
+  }, 1000 * 60 * 60);
 });
+
+const sendQuestion = async () => {
+  // var messageText = "";
+
+  // try {
+  //   chatId = req.body.message.chat.id;
+  //   messageText = req.body.message.text;
+  // } catch (err) {
+  //   // console.log(err);
+  // }
+
+  // try {
+  //   chatId = req.body.poll_answer.user.id;
+  // } catch (err) {
+  //   // console.log(err);
+  // }
+
+  // if (chatId === 0) return res.send("Mbolani!!!");
+
+  // if (
+  //   messageText !== "/new_question" &&
+  //   messageText !== "new_question" &&
+  //   messageText !== "new"
+  // ) {
+  //   axiosData.message =
+  //     'send or enter the command "/new_question" without the qotes to get a new question';
+  //   sendMessage(axiosData);
+  // } else {
+  //   // const questionInf = getQuestion();
+
+  // }
+
+  var chatId = process.env.CHAT_ID;
+  var axiosData = {};
+  axiosData.chat_id = chatId;
+  let content = JSON.parse(fs.readFileSync("questionsDB.json", "utf8"));
+
+  if (content.unusedQuestionsIndex.length === 0) {
+    for (var i = 0; i < content.questions.length; i++) {
+      content.unusedQuestionsIndex.push(i);
+    }
+  }
+
+  shuffle(content.unusedQuestionsIndex);
+  const numberOfQuestions =
+    content.unusedQuestionsIndex.length >= 5
+      ? 5
+      : content.unusedQuestionsIndex.length;
+
+  for (var i = 0; i < numberOfQuestions; i++) {
+    var questionIndex = content.unusedQuestionsIndex.pop();
+    axiosData.questionInf = content.questions[questionIndex];
+    await sendPoll(axiosData);
+  }
+
+  console.log(
+    `Number of question left: ${content.unusedQuestionsIndex.length}`
+  );
+
+  fs.writeFileSync("questionsDB.json", JSON.stringify(content));
+};
--- axios.js
+++ axios.js
@@ -1,29 +1,29 @@
 const axios = require("axios");
 require("dotenv").config();
 
-const sendPoll = (axiosData) => {
-  axios({
+const sendPoll = async (axiosData) => {
+  await axios({
     method: "post",
     url: `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendPoll`,
     data: {
       chat_id: axiosData.chat_id, //6926073558,
       question: axiosData.questionInf.question, // "What is the capital of Benin",
       options: axiosData.questionInf.options, //["Cotonou", "Porto-Novo", "Bénin-City"],
-      is_anonymous: false,
+      is_anonymous: true,
       type: "quiz",
       correct_option_id: axiosData.questionInf.correct_answer_index,
     },
   });
 };
 
-const sendMessage = (axiosData) =>
-  axios({
-    method: "post",
-    url: `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
-    data: {
-      chat_id: axiosData.chat_id,
-      text: axiosData.message,
-    },
-  });
+// const sendMessage = (axiosData) =>
+//   axios({
+//     method: "post",
+//     url: `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
+//     data: {
+//       chat_id: axiosData.chat_id,
+//       text: axiosData.message,
+//     },
+//   });
 
-module.exports = { sendMessage, sendPoll };
+module.exports = { /*sendMessage, */ sendPoll };
